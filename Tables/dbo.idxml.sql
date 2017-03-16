df5e65355fe93c2ea093a4d9ba84042b752ba6a8
VasiliyK <VasiliyK@Devart.com>
2017-03-16 17:21:37 +0200

    Closes #1

---
+++ Tables/dbo.idxml.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[idxml] (
+  [id] [int] IDENTITY,
+  [data] [xml] NULL,
+  PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Views/dbo.view1.sql
@@ -0,0 +1,5 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE VIEW [dbo].[view1] 
+AS select 1 AS odin
+GO
