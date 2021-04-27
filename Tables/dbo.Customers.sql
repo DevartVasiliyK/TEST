a7168fcf82a49286526e16420e011a2c17c4a216
SergeySor <SergeySor@devart.com>
2021-04-27 12:19:54 +0300

    0

---
+++ Tables/dbo.Customers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers] (
+  [custid] [char](11) NOT NULL,
+  [custname] [nvarchar](50) NOT NULL,
+  CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([custid])
+)
+ON [PRIMARY]
+GO
